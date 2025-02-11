35ee9987a9a43c279842c604c79bbc8a237e3df6
aaog2 <aungaungoogame2@gmail.com>
2025-02-11 16:26:13 +0630

    feat: update padding in Accordion component for improved layout

--- src/components/Home/Accordian.jsx
+++ src/components/Home/Accordian.jsx
@@ -45,7 +45,7 @@ const Accordion = () => {
   };
 
   return (
-    <div className="p-5 lg:p-0 md:py-[64px] mb-20 lg:w-[1000px] mx-auto">
+    <div className="p-5 md:py-[64px] mb-20 lg:w-[1000px] mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
         <div className="flex items-center mb-10 flex-col md:hidden">
           <img src={faq} alt="" className="w-64" />
